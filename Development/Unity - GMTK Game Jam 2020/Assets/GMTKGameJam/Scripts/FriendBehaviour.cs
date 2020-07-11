3961d78aef58df65b807a423a9b45b15160f494b
Keith Evans <keith.a.evans.1@gmail.com>
2020-07-12 00:53:27 +0200

    Adding a Friend. Friend is sad and needs cats to make them feel better Set their AmountOfCat to 0.5 and they stand up. Set it to 1 and they applaud. They revert to sad when there's less kitties around. They like poms, they just also like kitties and there's only one pom but so many kitties.

---
+++ Development/Unity - GMTK Game Jam 2020/Assets/GMTKGameJam/Scripts/FriendBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendBehaviour : MonoBehaviour
+{
+	public float AmountOfCat = 0;
+	public Animator animatorRef;
+	protected Camera m_Camera;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		 animatorRef.SetFloat("AmountOfCat", AmountOfCat);
+    }
+	 void LateUpdate() {
+		 transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+	 }
+}
